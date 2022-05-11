c4fcf069244fd622d671f68800e8c148a272863f
EWallGit <eiwall606@gmail.com>
2022-05-11 20:09:40 +0100

    PushingPiratesEmma
    yay

--- piratescript.js
+++ piratescript.js
@@ -1,6 +1,22 @@
-
-function piratize(input){
-
-    // add you code in here
-    
+function piratize(input){ 
+    const words = input.split(' '); //splits up the sentence into individual words
+    const translatedwords = [] 
+    for (let element of words) {
+        console.log(element);
+        if(element==="pounds"){
+            translatedwords.push("doubloons")
+        }
+        else if(Number(element)){ //will find numbers
+            translatedwords.push(element);
+        } 
+        else{
+            let ChangedWord=''; //created empty string called ChangedWord
+            let AnotherString=element.substring(1); //gives the word without the first letter
+            ChangedWord=AnotherString+element[0]+"arr"
+            translatedwords.push(ChangedWord);
+        }
+      }
+      let FinalArr='';
+      FinalArr=translatedwords.join(' ');
+      return FinalArr;
 } 
